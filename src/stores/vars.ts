1bf00409fb1d4236c473cd23a5f2cabb3bac4896
Elí Maciel <tecno_eli@hotmail.com>
2023-11-22 16:05:53 -0600

    migrated to astro

--- src/stores/vars.ts
+++
@@ -1,7 +0,0 @@
-import { writable } from "svelte/store";
-
-export const terminal = writable<string>('deno > run on http://localhost:3000/\ndeno > ')
-
-export function addTerminalLine(v: string) {
-    terminal.update(o => o+v+'\ndeno > ')
-}
