50c4703b9f5dadc36620f22528e77a65b1353d94
Elí Maciel <tecno_eli@hotmail.com>
2024-01-13 21:15:05 -0600

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
-import svelte, { vitePreprocess } from '@astrojs/svelte';
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(), 
-    svelte()
-  ],
-  preprocess: vitePreprocess(),
+  integrations: [tailwind(), svelte()],
+  site: 'https://eliyya.github.io'
 });
