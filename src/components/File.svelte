b8d096155c26d32db2f4d69a00bba9b6acaac18d
Elí Maciel <tecno_eli@hotmail.com>
2024-01-14 00:40:52 -0600

    add eslint

--- src/components/File.svelte
+++ src/components/File.svelte
@@ -8,28 +8,36 @@ interface Props {
 const { name, path, tabs } = Astro.props;
 --- -->
 <script lang="ts">
-    export let name: string;
-    export let path: string;
-    export let tabs: number;
+  export let name: string;
+  export let path: string;
+  export let tabs: number;
 </script>
 
 <a href={path}>
-    <span style={`padding-left: ${tabs * 4 + 18}px;`}>
-        {name}
-    </span>
+  <span style={`padding-left: ${tabs * 4}px;`}>
+    {name}
+  </span>
 </a>
 
 <style>
-    a {
-        padding-right: 0.5rem;
+  a {
+    padding-right: 0.5rem;
+  }
+
+  span {
+    display: flex;
+    gap: 0.25rem;
+
+    &:hover {
+      background-color: #37373d;
     }
 
-    span {
-        display: flex;
-        gap: 0.25rem;
-
-        &:hover {
-            background-color: #37373d;
-        }
+    &::before {
+      content: "";
+      width: 8px;
+      display: inline-block;
+      position: relative;
+      top: -1px;
     }
+  }
 </style>
