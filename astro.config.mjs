1c2cf95a4fc84520d7ce9ccae74afc89ab6fc556
Elí Maciel <tecno_eli@hotmail.com>
2024-01-14 22:18:31 -0600

    togle aside

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import svelte from "@astrojs/svelte";
+import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
-  site: 'https://eliyya.github.io'
-});
+    integrations: [tailwind({
+        nesting: true
+    }), svelte()],
+    site: 'https://eliyya.github.io'
+})
